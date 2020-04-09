fc25175c1c4847faa68ea64ff9ee7b5ca250eb94
CDelaney0 <46506898+CDelaney0@users.noreply.github.com>
2020-04-09 17:08:48 -0600

    Update FlickrApi.kt

--- Photogallery/app/src/main/java/com/example/photogallery/api/FlickrApi.kt
+++ Photogallery/app/src/main/java/com/example/photogallery/api/FlickrApi.kt
@@ -7,14 +7,13 @@ import retrofit2.http.Url
 
 interface FlickrApi {
 
-    @GET(
-        "services/rest/?method=flickr.interestingness.getList" +
-                "&api_key=660c3f34e0bc88113bedc20b2960abc2" +
-                "&format=json" +
-                "&nojsoncallback=1" +
-                "&extras=url_s"
-    )
+    @GET("services/rest/?method=flickr.interestingness.getList" +
+            "&api_key=4f721bbafa75bf6d2cb5af54f937bb70" +
+            "&format=json" +
+            "&nojsoncallback=1" +
+            "&extras=url_s")
     fun fetchPhotos(): Call<FlickrResponse>
+
     @GET
     fun fetchUrlBytes(@Url url: String): Call<ResponseBody>
 }
